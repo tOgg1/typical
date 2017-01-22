8fa935bdbab2dc80d389542ddeaec08c3ef35ffb
Tormod Haugland <tormod.haugland@gmail.com>
2017-01-22 11:38:29 +0100

    Added configWriter
    The configWriter takes a config-object, and from it writes the specified files/directories to the current directory of invocation.

---
+++ src/configWriter.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const ncp = require('ncp').ncp
+const path = require('path')
+
+function writeFile (path, content) {
+  fs.writeFile(path, content, 'utf8', error => {
+    if (error) {
+      throw error
+    }
+  })
+}
+
+function writeFiles (parentDirectory, files) {
+  if (!files) {
+    return
+  }
+  if (files.constructor === Array) {
+    files.forEach(file => writeFile(path.resolve(parentDirectory, file), ''))
+  } else if (files.constructor === Object) {
+    Object.keys(files).forEach(name => {
+      writeFile(name, files[name])
+    })
+  }
+}
+
+function writeDirectory (parentDirectory, directoryObject) {
+  writeFiles(parentDirectory, directoryObject.files)
+  if (!directoryObject.directories) return
+  Object.keys(directoryObject.directories).forEach(directory => {
+    const newParentDirectory = parentDirectory + '/' + directory
+    fs.mkdirSync(newParentDirectory)
+    writeDirectory(newParentDirectory, directoryObject.directories[directory])
+  })
+}
+
+function writeFolderConfig (config, callback) {
+  const loadFromPath = config.path
+  ncp(loadFromPath, process.cwd(), error => {
+    if (error) {
+      throw error
+      callback()
+    }
+  })
+}
+
+function writeRegularConfig (config) {
+  return writeDirectory(process.cwd(), config)
+}
+
+function write (config, callback) {
+  if (config.isDirectory) {
+    writeFolderConfig(config, callback)
+  } else {
+    writeRegularConfig(config)
+  }
+}
+
+module.exports = {
+  write: write
+}
