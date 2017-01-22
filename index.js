be1173bf5e4edcd61522333343d1fc2ccbb31ec3
Tormod Haugland <tormod.haugland@gmail.com>
2017-01-22 11:42:36 +0100

    Added the index.js file
    The file contains a minimal cli for dealing with invocation of the program

---
+++ index.js
@@ -0,0 +1,26 @@
+let program = require('commander')
+let configResolver = require('./src/configResolver')
+let configWriter = require('./src/configWriter')
+
+let configElement = '_default'
+
+// Set up and parse program arguments.
+// We only really care about the first positional argument
+// giving use the correct config to use
+program
+  .arguments('[configElement]')
+  .action(_configElement => {
+    configElement = _configElement
+  })
+  .parse(process.argv)
+
+// Merge regular config file and folder config
+let config = Object.assign(
+  {},
+  configResolver.resolve(),
+  configResolver.resolveFolderConfig()
+)
+
+// Lets go.
+configWriter.write(config[configElement])
+
