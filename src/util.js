54e39100ae912e6c20e9f03a24ac0b6c8aa7a8f6
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-13 07:22:31 +0200

    Update util.js
    Add method #containsIgnoredPattern, which tests if a string contains any of the ignored patterns in the ignoredPatterns variable

--- src/util.js
+++ src/util.js
@@ -1,7 +1,18 @@
 const ignoredPatterns = [
-  '__meta__',
-  '__interpolations__'
+  /__meta__/,
+  /__interpolations__/,
+  /__hooks__/,
+  /__isDirectory__/
 ]
+
+function containsIgnoredPattern (string) {
+  for (var i = 0; i < ignoredPatterns.length; i++) {
+    if (string.match(ignoredPatterns[i])) {
+      return true
+    }
+  }
+  return false
+}
 
 // Taken from https://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
 function escapeRegExp (str) {
@@ -13,5 +24,6 @@ const interpolationRegex = /\$\$\{([\w0-9]+)\}/g
 module.exports = {
   escapeRegExp: escapeRegExp,
   ignoredPatterns: ignoredPatterns,
+  containsIgnoredPattern: containsIgnoredPattern,
   interpolationRegex: interpolationRegex
 }
